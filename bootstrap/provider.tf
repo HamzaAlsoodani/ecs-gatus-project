15848f5ce198a82affbf1aa3e04705d61622bab8
HamzaAlsoodani <alsoodanihamza6@gmail.com>
2026-07-02 01:15:46 +0100

    updated bootstrap provider.tf

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -1,6 +1,4 @@
 terraform {
-  # WHAT: Declares the providers this Terraform configuration depends on.
-  # WHY: Terraform needs to know which plugins to install before it can create AWS resources.
   required_providers {
     aws = {
       source  = "hashicorp/aws"
